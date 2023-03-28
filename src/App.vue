cdaf56e9dcbd1cdee8cf795a0862e8122c8d737f
MohamedElmdary <engm5081@gmail.com>
2023-03-28 16:56:31 +0200

    Add weblet layout

--- src/App.vue
+++ src/App.vue
@@ -7,7 +7,42 @@
     </v-navigation-drawer>
 
     <v-main>
-      <ProfileManager />
+      <WebletLayout>
+        <template #title> Profile Manager </template>
+        <template #subtitle> asdasd </template>
+        <template #header-actions>asd</template>
+
+        Lorem ipsum dolor sit amet consectetur adipisicing elit. Asperiores nemo veniam qui
+        accusantium alias perspiciatis distinctio facilis, commodi at sapiente eius quod itaque
+        harum vel ullam dignissimos repudiandae quasi eum doloribus tenetur accusamus eveniet.
+        Quisquam architecto atque, tempore voluptatem aliquam nulla veritatis a nemo in maiores
+        mollitia totam recusandae eligendi expedita soluta laboriosam illo nihil laudantium earum.
+        Ex, placeat facere praesentium iste officiis culpa impedit corporis natus quas maiores animi
+        consequuntur voluptatum tempore. Voluptatum repellat aperiam mollitia cumque consequuntur
+        quam voluptas, temporibus quisquam nesciunt necessitatibus vero. Accusantium suscipit
+        ratione, repellendus praesentium enim dolor nesciunt quo, voluptatibus tempora laudantium
+        alias, cum veritatis magni doloribus animi ipsum quia cupiditate porro facere. Aut nulla
+        beatae ipsam corrupti a incidunt inventore veniam obcaecati assumenda saepe, neque rerum
+        explicabo asperiores pariatur accusamus nihil placeat eaque velit magni alias qui odio
+        delectus officiis. Nesciunt harum cum doloribus, sit cupiditate dolorem culpa placeat!
+        Cupiditate dolorum eius recusandae dolorem ullam dignissimos voluptates, ipsam quisquam!
+        Explicabo, numquam similique doloribus at accusamus excepturi quasi! Unde, laboriosam
+        dolorum. Blanditiis at necessitatibus incidunt voluptate a neque adipisci voluptas
+        voluptates. Officiis, sequi vero? Quasi odit cum exercitationem cupiditate animi totam
+        rerum, commodi adipisci! Exercitationem doloremque nam nobis sunt? Rerum maxime officiis qui
+        porro vitae hic repellendus doloribus architecto eveniet, ducimus adipisci doloremque a
+        quam? Cumque tenetur eos, optio nisi incidunt corrupti alias, ut fugiat quisquam doloremque
+        neque? Rem est ea, provident exercitationem dignissimos odio voluptatum hic voluptatibus
+        eligendi vitae vero ratione temporibus nemo expedita. Consectetur tempore animi ducimus,
+        excepturi minus quam non? Asperiores consectetur quae ullam explicabo qui reiciendis fugit
+        veniam ipsa aut, cum quam ab perferendis praesentium dolor minus, maiores, iusto totam vitae
+        deserunt? Dolores est quod voluptatem ipsum commodi officia officiis tenetur distinctio
+        quos. Voluptatibus, sapiente amet eius labore totam accusamus. Ducimus nemo harum aspernatur
+        excepturi error tempore at tempora impedit?
+
+        <template #footer-actions>sad</template>
+      </WebletLayout>
+      <!-- <ProfileManager /> -->
     </v-main>
   </v-app>
 </template>
@@ -15,11 +50,11 @@
 <script lang="ts" setup></script>
 
 <script lang="ts">
-import ProfileManager from './weblets/profile_manager.vue'
+import WebletLayout from './components/weblet_layout.vue'
 
 export default {
   components: {
-    ProfileManager
+    WebletLayout
   }
 }
 </script>
---
+++ src/components/weblet_layout.vue
@@ -0,0 +1,36 @@
+<template>
+  <v-card>
+    <section class="d-flex align-center">
+      <div>
+        <v-card-title v-if="$slots.title">
+          <slot name="title" />
+        </v-card-title>
+        <v-card-subtitle v-if="$slots.subtitle">
+          <slot name="subtitle" />
+        </v-card-subtitle>
+      </div>
+      <v-spacer />
+      <div class="mr-4" v-if="$slots['header-actions']">
+        <slot name="header-actions" />
+      </div>
+    </section>
+
+    <v-divider class="mt-5 mb-2" />
+
+    <v-card-text> <slot /> </v-card-text>
+
+    <template v-if="$slots['footer-actions']">
+      <v-divider class="mt-5" />
+      <v-card-actions>
+        <v-spacer />
+        <slot name="footer-actions" />
+      </v-card-actions>
+    </template>
+  </v-card>
+</template>
+
+<script lang="ts">
+export default {
+  name: 'WebletLayout'
+}
+</script>
