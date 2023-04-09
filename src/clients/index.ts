e4b2a74313c85e9fdebe711a3352ea5f7685bf64
MohamedElmdary <engm5081@gmail.com>
2023-04-09 11:38:32 +0200

    Allow to pass network env while serving app

--- src/clients/index.ts
+++ src/clients/index.ts
@@ -1,7 +1,7 @@
 import TFGridGqlClient, { Networks } from 'tfgrid-gql'
 import GridProxyClient, { Network } from 'tf_gridproxy_client'
 
-const gqlClient = new TFGridGqlClient(Networks.Dev)
-const gridProxyClient = new GridProxyClient(Network.Dev)
+const gqlClient = new TFGridGqlClient(process.env.NETWORK as Networks)
+const gridProxyClient = new GridProxyClient(process.env.NETWORK as Network)
 
 export { gqlClient, gridProxyClient }
