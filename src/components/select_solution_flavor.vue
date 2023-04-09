72d98fd985a6864f7478dfc781e0fa4857411878
MohamedElmdary <engm5081@gmail.com>
2023-04-09 12:09:50 +0200

    Allow users to add there package with a fallback with standard one

--- src/components/select_solution_flavor.vue
+++ src/components/select_solution_flavor.vue
@@ -20,35 +20,51 @@
       @input="$emit('update:disk', $event.target.value)"
     />
   </div>
+
+  {{ $props }}
 </template>
 
 <script lang="ts" setup>
-import { ref, watch } from 'vue'
+import type { PropType } from 'vue'
+import { ref, watch, computed } from 'vue'
 
-defineProps<{ cpu?: number; memory?: number; disk?: number }>()
+type Package = PropType<{ cpu: number; memory: number; disk: number }>
+
+const props = defineProps({
+  cpu: { required: true, type: Number },
+  memory: { required: true, type: Number },
+  disk: { required: true, type: Number },
+  minimum: { type: Object as Package, default: () => ({ cpu: 1, memory: 1024, disk: 100 }) },
+  standard: { type: Object as Package, default: () => ({ cpu: 2, memory: 1024 * 2, disk: 250 }) },
+  recommended: { type: Object as Package, default: () => ({ cpu: 4, memory: 1024 * 4, disk: 500 }) }
+})
+
 const $emit = defineEmits<{
   (event: 'update:cpu', value?: number): void
   (event: 'update:memory', value?: number): void
   (event: 'update:disk', value?: number): void
 }>()
 
-const packages = [
-  {
-    title: 'Minimum(CPU: 1 vCores, Memory: 1024 MB, Disk: 100 GB)',
-    value: { cpu: 1, memory: 1024, disk: 100 }
-  },
-  {
-    title: 'Standard(CPU: 2 vCores, Memory: 2048 MB, Disk: 250 GB)',
-    value: { cpu: 2, memory: 1024 * 2, disk: 250 }
-  },
-  {
-    title: 'Recommended(CPU: 4 vCores, Memory: 4096 MB, Disk: 500 GB)',
-    value: { cpu: 4, memory: 1024 * 4, disk: 500 }
-  },
-  { title: 'Custom', value: 'custom' }
-]
+const packages = computed(() => {
+  const { minimum, standard, recommended } = props
+  return [
+    {
+      title: `Minimum(CPU: ${minimum.cpu} vCores, Memory: ${minimum.memory} MB, Disk: ${minimum.disk} GB)`,
+      value: minimum
+    },
+    {
+      title: `Standard(CPU: ${standard.cpu} vCores, Memory: ${standard.memory} MB, Disk: ${standard.disk} GB)`,
+      value: standard
+    },
+    {
+      title: `Recommended(CPU: ${recommended.cpu} vCores, Memory: ${recommended.memory} MB, Disk: ${recommended.disk} GB)`,
+      value: recommended
+    },
+    { title: 'Custom', value: 'custom' }
+  ]
+})
 
-const solution = ref(packages[0].value)
+const solution = ref(packages.value[0].value)
 
 watch(
   solution,
