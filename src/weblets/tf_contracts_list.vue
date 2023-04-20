83bedc5b1b253a9f24f836562fc3e96e96fd268a
MohamedElmdary <engm5081@gmail.com>
2023-04-20 12:42:52 +0200

    Add contracts list files

---
+++ src/weblets/tf_contracts_list.vue
@@ -0,0 +1,13 @@
+<template>
+  <div>Contracts List</div>
+</template>
+
+<script lang="ts" setup>
+console.log('setup')
+</script>
+
+<script lang="ts">
+export default {
+  name: 'TfContractsList'
+}
+</script>
