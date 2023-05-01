f46165a28d20237ea2c6276a5d92bd9c54e206f4
zaelgohary <zainabelgohary19@gmail.com>
2023-05-01 11:39:57 +0300

    edit micro vm

--- src/weblets/micro_vm.vue
+++ src/weblets/micro_vm.vue
@@ -18,166 +18,147 @@
         { title: 'Environment Variables', value: 'env', invalid: !isEnvsValid },
         { title: 'Disks', value: 'disks', invalid: !isDisksValid }
       ]"
+      ref="tabs"
     >
       <template #config>
-        <form-validator v-model="isConfigValid">
-          <template #default="{ form }">
-            <input-validator
-              v-bind="form"
-              :value="name"
-              :rules="[
-                validators.required('Name is required.'),
-                validators.minLength('Name minLength is 2 chars.', 2),
-                validators.maxLength('Name maxLength is 15 chars.', 15)
-              ]"
-            >
-              <template #default="{ props }">
-                <v-text-field label="Name" v-model="name" v-bind="props" />
-              </template>
-            </input-validator>
+        <input-validator
+          :value="name"
+          :rules="[
+            validators.required('Name is required.'),
+            validators.minLength('Name minLength is 2 chars.', 2),
+            validators.maxLength('Name maxLength is 15 chars.', 15)
+          ]"
+        >
+          <template #default="{ props }">
+            <v-text-field label="Name" v-model="name" v-bind="props" />
+          </template>
+        </input-validator>
 
-            <SelectVmImage
-              :images="images"
-              v-model:flist="flist"
-              v-model:entry-point="entryPoint"
-              :form="form"
-            />
+        <SelectVmImage
+          :images="images"
+          v-model:flist="flist"
+          v-model:entry-point="entryPoint"
+        />
 
-            <RootFsSize v-model="rootFsSize" :form="form" />
+        <RootFsSize v-model="rootFsSize" />
 
-            <input-validator
-              v-bind="form"
-              :value="cpu"
-              :rules="[
-                validators.required('CPU is required.'),
-                validators.isInt('CPU must be a valid integer.'),
-                validators.min('CPU min is 2 cores.', 2),
-                validators.max('CPU max is 32 cores.', 32)
-              ]"
-            >
-              <template #default="{ props }">
-                <v-text-field
-                  label="CPU (vCores)"
-                  type="number"
-                  v-model.number="cpu"
-                  v-bind="props"
-                />
-              </template>
-            </input-validator>
-
-            <input-validator
-              v-bind="form"
-              :value="memory"
-              :rules="[
-                validators.required('Memory is required.'),
-                validators.isInt('Memory must be a valid integer.'),
-                validators.min('Minimum allowed memory is 256 MB.', 256),
-                validators.max('Maximum allowed memory is 256 GB.', 256 * 1024)
-              ]"
-            >
-              <template #default="{ props }">
-                <v-text-field
-                  label="Memory (MB)"
-                  type="number"
-                  v-model.number="memory"
-                  v-bind="props"
-                />
-              </template>
-            </input-validator>
-
-            <v-switch color="primary" inset label="Public IPv4" v-model="ipv4" />
-            <v-switch color="primary" inset label="Public IPv6" v-model="ipv6" />
-            <v-switch color="primary" inset label="Planetary Network" v-model="planetary" />
-            <v-switch color="primary" inset label="Add Wireguard Access" v-model="wireguard" />
-
-            <SelectFarm
-              :filters="{
-                cpu,
-                memory,
-                publicIp: ipv4,
-                ssd: disks.reduce((total, disk) => total + disk.size, rootFsSize)
-              }"
-              v-model="farm"
-              v-model:country="country"
-              :form="form"
+        <input-validator
+          :value="cpu"
+          :rules="[
+            validators.required('CPU is required.'),
+            validators.isInt('CPU must be a valid integer.'),
+            validators.min('CPU min is 2 cores.', 2),
+            validators.max('CPU max is 32 cores.', 32)
+          ]"
+        >
+          <template #default="{ props }">
+            <v-text-field
+              label="CPU (vCores)"
+              type="number"
+              v-model.number="cpu"
+              v-bind="props"
             />
           </template>
-        </form-validator>
+        </input-validator>
+
+        <input-validator
+          :value="memory"
+          :rules="[
+            validators.required('Memory is required.'),
+            validators.isInt('Memory must be a valid integer.'),
+            validators.min('Minimum allowed memory is 256 MB.', 256),
+            validators.max('Maximum allowed memory is 256 GB.', 256 * 1024)
+          ]"
+        >
+          <template #default="{ props }">
+            <v-text-field
+              label="Memory (MB)"
+              type="number"
+              v-model.number="memory"
+              v-bind="props"
+            />
+          </template>
+        </input-validator>
+
+        <v-switch color="primary" inset label="Public IPv4" v-model="ipv4" />
+        <v-switch color="primary" inset label="Public IPv6" v-model="ipv6" />
+        <v-switch color="primary" inset label="Planetary Network" v-model="planetary" />
+        <v-switch color="primary" inset label="Add Wireguard Access" v-model="wireguard" />
+
+        <SelectFarm
+          :filters="{
+            cpu,
+            memory,
+            publicIp: ipv4,
+            ssd: disks.reduce((total, disk) => total + disk.size, rootFsSize)
+          }"
+          v-model="farm"
+          v-model:country="country"
+        />
       </template>
 
       <template #env>
-        <form-validator v-model='isEnvsValid'>
-          <template #default=' { form }'>
-            <ExpandableLayout v-model="envs" @add="envs.push({ key: '', value: '' })">
-              <template #default="{ index }">
-                <input-validator
-                    v-bind="form"
-                    :value="envs[index].key"
-                    :rules="[
-                      validators.required('Key name is required.'),
-                      validators.validateKey('Key can\'t start with a number, a non-alphanumeric character or a whitespace'),
-                      validators.maxLength('Key max length is 128 chars.', 128)
-                    ]"
-                  >
-                  <template #default="{ props }">
-                    <v-text-field label="Name" v-model="envs[index].key" v-bind="props" />
-                  </template>
-                </input-validator>
-
-                <v-textarea label="Value" v-model="envs[index].value" no-resize :spellcheck="false" />
+        <ExpandableLayout v-model="envs" @add="envs.push({ key: '', value: '' })">
+          <template #default="{ index }">
+            <input-validator
+                :value="envs[index].key"
+                :rules="[
+                  validators.required('Key name is required.'),
+                  validators.validateKey('Key can\'t start with a number, a non-alphanumeric character or a whitespace'),
+                  validators.maxLength('Key max length is 128 chars.', 128)
+                ]"
+              >
+              <template #default="{ props }">
+                <v-text-field label="Name" v-model="envs[index].key" v-bind="props" />
               </template>
-            </ExpandableLayout>
+            </input-validator>
+
+            <v-textarea label="Value" v-model="envs[index].value" no-resize :spellcheck="false" />
           </template>
-        </form-validator>
+        </ExpandableLayout>
       </template>
 
       <template #disks>
-        <form-validator v-model="isDisksValid">
-          <template #default=' { form }'>
-            <ExpandableLayout v-model="disks" @add="addDisk">
-                <template #default="{ index }">
-                  <p class="text-h6 mb-4">Disk #{{ index + 1 }}</p>
-                  <input-validator
-                    v-bind="form"
-                    :value="disks[index].name"
-                    :rules="[
-                      validators.required('Disk name is required.'),
-                      validators.pattern(
-                        'Disk name can\'t start with a number, a non-alphanumeric character or a whitespace',
-                        { pattern: /^[A-Za-z]/ }
-                      ),
-                      validators.minLength('Disk minLength is 2 chars.', 2),
-                      validators.isAlphanumeric('Disk name only accepts alphanumeric chars.'),
-                      validators.maxLength('Disk maxLength is 15 chars.', 15)
-                    ]"
-                  >
-                    <template #default="{ props }">
-                      <v-text-field label="Name" v-model="disks[index].name" v-bind="props" />
-                    </template>
-                  </input-validator>
-                  <input-validator
-                    v-bind="form"
-                    :value="disks[index].size"
-                    :rules="[
-                      validators.required('Disk size is required.'),
-                      validators.isInt('Disk size must be a valid integer.'),
-                      validators.min('Minimum allowed disk size is 1 GB.', 1),
-                      validators.max('Maximum allowed disk size is 10000 GB.', 10000)
-                    ]"
-                  >
-                    <template #default="{ props }">
-                      <v-text-field
-                        label="Size (GB)"
-                        type="number"
-                        v-model.number="disks[index].size"
-                        v-bind="props"
-                      />
-                    </template>
-                  </input-validator>
+        <ExpandableLayout v-model="disks" @add="addDisk">
+            <template #default="{ index }">
+              <p class="text-h6 mb-4">Disk #{{ index + 1 }}</p>
+              <input-validator
+                :value="disks[index].name"
+                :rules="[
+                  validators.required('Disk name is required.'),
+                  validators.pattern(
+                    'Disk name can\'t start with a number, a non-alphanumeric character or a whitespace',
+                    { pattern: /^[A-Za-z]/ }
+                  ),
+                  validators.minLength('Disk minLength is 2 chars.', 2),
+                  validators.isAlphanumeric('Disk name only accepts alphanumeric chars.'),
+                  validators.maxLength('Disk maxLength is 15 chars.', 15)
+                ]"
+              >
+                <template #default="{ props }">
+                  <v-text-field label="Name" v-model="disks[index].name" v-bind="props" />
                 </template>
-              </ExpandableLayout>
-          </template>
-        </form-validator>
+              </input-validator>
+              <input-validator
+                :value="disks[index].size"
+                :rules="[
+                  validators.required('Disk size is required.'),
+                  validators.isInt('Disk size must be a valid integer.'),
+                  validators.min('Minimum allowed disk size is 1 GB.', 1),
+                  validators.max('Maximum allowed disk size is 10000 GB.', 10000)
+                ]"
+              >
+                <template #default="{ props }">
+                  <v-text-field
+                    label="Size (GB)"
+                    type="number"
+                    v-model.number="disks[index].size"
+                    v-bind="props"
+                  />
+                </template>
+              </input-validator>
+            </template>
+        </ExpandableLayout>
       </template>
     </d-tabs>
 
@@ -199,6 +180,7 @@ import { getGrid } from '../utils/grid'
 import * as validators from '../utils/validators'
 
 const layout = ref()
+const tabs = ref()
 const profileManager = useProfileManager()
 
 const isConfigValid = ref(false)
