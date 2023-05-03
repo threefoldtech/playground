04a8df50483f0873e735469f3105fa3eb617ad7b
MohamedElmdary <engm5081@gmail.com>
2023-05-03 18:18:09 +0300

    Add funkwhale files

---
+++ src/views/funkwhale_view.vue
@@ -0,0 +1,14 @@
+<template>
+  <TfFunkwhale />
+</template>
+
+<script lang="ts">
+import TfFunkwhale from '../weblets/tf_funkwhale.vue'
+
+export default {
+  name: 'FunkwhaleView',
+  components: {
+    TfFunkwhale,
+  },
+}
+</script>
---
+++ src/weblets/tf_funkwhale.vue
@@ -0,0 +1,11 @@
+<template>TfFunkwhale</template>
+
+<script lang="ts" setup>
+console.log('TfFunkwhale')
+</script>
+
+<script lang="ts">
+export default {
+  name: 'TfFunkwhale',
+}
+</script>
