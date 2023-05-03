85690c94544884b41c08016207c543c54ffb9065
MohamedElmdary <engm5081@gmail.com>
2023-05-03 17:54:41 +0300

    Add main peertube files

---
+++ src/views/peertube_view.vue
@@ -0,0 +1,14 @@
+<template>
+  <TfPeertube />
+</template>
+
+<script lang="ts">
+import TfPeertube from '../weblets/tf_peertube.vue'
+
+export default {
+  name: 'PeertubeView',
+  components: {
+    TfPeertube,
+  },
+}
+</script>
---
+++ src/weblets/tf_peertube.vue
@@ -0,0 +1,11 @@
+<template>TfPeertube</template>
+
+<script lang="ts" setup>
+console.log('TfPeertube')
+</script>
+
+<script lang="ts">
+export default {
+  name: 'TfPeertube',
+}
+</script>
