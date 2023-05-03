25ab739f7c5d3ec8b29262e8b43e6049ddff9b5a
MohamedElmdary <engm5081@gmail.com>
2023-05-03 14:19:52 +0300

    Add main files

--- src/router/index.ts
+++ src/router/index.ts
@@ -6,39 +6,44 @@ const router = createRouter({
     {
       path: '/',
       component: () => import('../views/full_virtual_machine.vue'),
-      meta: { title: 'Full Virtual Machine' }
+      meta: { title: 'Full Virtual Machine' },
     },
     {
       path: '/vm',
       component: () => import('../views/micro_virtual_machine.vue'),
-      meta: { title: 'Micro Virtual Machine' }
+      meta: { title: 'Micro Virtual Machine' },
     },
     {
       path: '/kubernetes',
       component: () => import('../views/kubernetes_view.vue'),
-      meta: { title: 'Kubernetes' }
+      meta: { title: 'Kubernetes' },
+    },
+    {
+      path: '/caprover',
+      component: () => import('../views/caprover_view.vue'),
+      meta: { title: 'Caprover' },
     },
     {
       path: '/presearch',
       component: () => import('../views/presearch_view.vue'),
-      meta: { title: 'Presearch' }
+      meta: { title: 'Presearch' },
     },
     {
       path: '/contractslist',
       component: () => import('../views/contracts_list.vue'),
-      meta: { title: 'Contracts List' }
+      meta: { title: 'Contracts List' },
     },
     {
       path: '/deployedlist',
       component: () => import('../views/deployment_list.vue'),
-      meta: { title: 'Deployment List' }
+      meta: { title: 'Deployment List' },
     },
     {
       path: '/:pathMatch(.*)*',
       component: () => import('../views/not_yet_implemented.vue'),
-      meta: { title: 'Not Yet Implemented' }
-    }
-  ]
+      meta: { title: 'Not Yet Implemented' },
+    },
+  ],
 })
 
 export default router
---
+++ src/weblets/tf_caprover.vue
@@ -0,0 +1,11 @@
+<template>tf_caprover</template>
+
+<script lang="ts" setup>
+console.log('caprover')
+</script>
+
+<script lang="ts">
+export default {
+  name: 'TfCaprover',
+}
+</script>
