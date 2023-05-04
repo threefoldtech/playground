4a49fd0fdd5f37908037eba5469a6d8dd750c153
MohamedElmdary <engm5081@gmail.com>
2023-05-04 13:09:09 +0300

    Add main files for mattermost

---
+++ src/views/mattermost_view.vue
@@ -0,0 +1,14 @@
+<template>
+  <TfMattermost />
+</template>
+
+<script lang="ts">
+import TfMattermost from '../weblets/tf_mattermost.vue'
+
+export default {
+  name: 'MattermostView',
+  components: {
+    TfMattermost,
+  },
+}
+</script>
---
+++ src/weblets/tf_mattermost.vue
@@ -0,0 +1,7 @@
+<template>tf_mattermost</template>
+
+<script lang="ts">
+export default {
+  name: 'TfMattermost',
+}
+</script>
