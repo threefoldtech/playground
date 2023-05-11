2060ca090cc6731900bfd19fcec25967b6031a38
MohamedElmdary <engm5081@gmail.com>
2023-05-11 11:53:42 +0300

    Add files to deploy on github pages

--- vite.config.ts
+++ vite.config.ts
@@ -6,16 +6,19 @@ import { nodePolyfills } from 'vite-plugin-node-polyfills'
 
 // https://vitejs.dev/config/
 export default defineConfig({
+  build: {
+    outDir: 'docs',
+  },
   plugins: [vue(), nodePolyfills()],
   resolve: {
     alias: {
-      '@': fileURLToPath(new URL('./src', import.meta.url))
-    }
+      '@': fileURLToPath(new URL('./src', import.meta.url)),
+    },
   },
   define: {
     'process.env': {
       NETWORK: process.env.NETWORK || 'dev',
-      INTERNAL_SOLUTION_PROVIDER_ID: process.env.INTERNAL_SOLUTION_PROVIDER_ID || 1
-    }
-  }
+      INTERNAL_SOLUTION_PROVIDER_ID: process.env.INTERNAL_SOLUTION_PROVIDER_ID || 1,
+    },
+  },
 })
